83ba3207649276beaa3cafc7b640affd452c26a4
Jakub <jakub.kucaba@gmail.com>
2023-07-11 12:38:15 +0200

    dodanie palety

--- app/src/main/java/com/example/mydrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/mydrawingapp/MainActivity.kt
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView : DrawingView? = null
+    private var mImageCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_presed)
+        )
 
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
